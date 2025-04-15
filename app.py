51ae55c919f4832ccf2df9c91f111a4291e66922
Vinicius Alves Camilo Beltrão <vinicius.beltrao@hrgama.com>
2025-04-15 12:16:39 -0300

    Corrige app para rodar no Render com porta pública

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,8 @@ def receber_webhook():
     dados = request.json
     print("📩 Dados recebidos do Webhook:")
     print(dados)
-
     return jsonify({"mensagem": "Webhook recebido com sucesso"}), 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Porta que o Render fornece
+    app.run(host='0.0.0.0', port=port)        # Deixa o app acessível publicamente
